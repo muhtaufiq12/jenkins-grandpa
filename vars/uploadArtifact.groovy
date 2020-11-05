64783cc229eaa485677115800426e51bc78f9fab
muhtaufiq12 <taufiqurrahmanm70@gmail.com>
2020-11-05 14:13:51 +0700

    repair bugs uploadArtifactory-20

--- vars/uploadArtifact.groovy
+++ vars/uploadArtifact.groovy
@@ -27,7 +27,7 @@ def call(Map param) {
                     }
                 }
                 steps {
-                    sh "maven compile -o"
+                    sh "mvn compile -o"
                 }
             }
 
