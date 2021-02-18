ed3788e484355c9b235635511ff37248f56a44f1
muhtaufiq12 <taufiqurrahmanm70@gmail.com>
2021-02-18 09:09:41 +0700

    remove comment

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,31 +1,11 @@
 #!/usr/bin/env groovy
 def call(Map params){
-    // def build = libraryResource 'build.sh'
     def prepareDeliver = libraryResource 'prepareDeliver.sh'
     def deliver = libraryResource 'deliver.sh'
     def packageAndShip = libraryResource 'packageAndShip.sh'
     pipeline {
         agent any
         stages {
-        //     stage('Build and Push Docker Image') {
-        //         steps {
-        //             withCredentials([
-        //                 usernamePassword(
-        //                     credentialsId: 'cred-docker',
-        //                     usernameVariable: 'DOCKER_USER',
-        //                     passwordVariable: 'DOCKER_PASSWORD'
-        //                 )
-        //             ]) {
-        //                 withEnv([
-        //                     'CONTAINER_REGISTRY='+params.containerRegistry,
-        //                     'CONTAINER_IMAGE='+params.containerImage,
-        //                     'CONTAINER_VERSION='+params.containerVersion,
-        //                 ]){
-        //                     sh(build)
-        //                 }
-        //             }
-        //         }
-        //     }
             stage('Package Application with HELM') {
                 agent {
                     docker {
